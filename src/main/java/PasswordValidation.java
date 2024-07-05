5f57a5aa851edecfed7eb845898ea7a4a77f5207
Olgasyla <olgasyla@gmail.com>
2024-07-05 09:06:22 +0200

    Pass_Validation_Check_Digit

--- src/main/java/PasswordValidation.java
+++ src/main/java/PasswordValidation.java
@@ -3,14 +3,23 @@
 public class PasswordValidation {
     public static void main(String[] args) {
 
-
     }
     // Method to Ensure Password is at Least 8 Characters Long
   public static boolean checkLength( String password) {
-        if (password.length() == 0) {
-            return false;
-        }
-            return password.length() >= 8;
-  }
-   // With another method, you want to check if the password contains digits
-}
+        if (password.length() >= 8) {
+            return true;
+        } return false; // return password.lenght ()>=8;
+      }
+
+  // With another method, you want to check if the password contains digits
+    public static boolean checkDigit( String password) {
+        return password.matches(".*[0-9].*");
+    }
+    public static boolean checkUpperCase( String password) {
+        return password.matches(".*[A-Z].*");
+    }
+    public static boolean checkLowerCase( String password) {
+        return password.matches(".*[a-z].*");
+
+
+    }}
--- src/test/java/PasswordValidationTest.java
+++ src/test/java/PasswordValidationTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class PasswordValidationTest {
 
     @Test
-    public void isPasswordLongEight_thenReturnTrue(){
+    public void isPasswordLongEight_thenReturnTrue() {
 
         //GIVEN
         String password = "password";
@@ -16,7 +16,56 @@ class PasswordValidationTest {
         //THEN
         assertTrue(actuel);
     }
-    public void isPasswordLongEight_thenReturnFalse() {
 
+    @Test
+    public void isPasswordLessEight_thenReturnFalse() {
+
+        //GIVEN
+        String password = "pass";
+
+        //WHEN
+        boolean actuel = PasswordValidation.checkLength(password);
+
+        //THEN
+        assertFalse(actuel);
+    }
+
+    @Test
+    public void isPasswordContainsDigit_thenReturnTrue() {
+
+        //GIVEN
+        String password = "password1";
+
+        //WHEN
+        boolean actuel = PasswordValidation.checkDigit(password);
+
+        //THEN
+        assertTrue(actuel);
+    }
+
+    @Test
+    public void isPasswordContainsUpperCase_thenReturnTrue() {
+
+        //GIVEN
+        String password = "Password";
+
+        //WHEN
+        boolean actuel = PasswordValidation.checkUpperCase(password);
+
+        //THEN
+        assertTrue(actuel);
+    }
+
+    @Test
+    public void isPasswordContainsLowerCase_thenReturnTrue() {
+
+        //GIVEN
+        String password = "Password";
+
+        //WHEN
+        boolean actuel = PasswordValidation.checkLowerCase(password);
+
+        //THEN
+        assertTrue(actuel);
     }
 }
